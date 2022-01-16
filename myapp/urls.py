fcb0d652ef6ce2e9f96b904280ec8bc321230573
CASSIO645 <cantonio645@gmail.com>
2022-01-16 16:06:08 -0300

    template and list view

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views 
+from . import views
 
+app_name = "employee"
 urlpatterns = [ 
-    
+    path("", views.EmployeeList.as_view(), name='list')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Employee
 
-# Create your views here.
+class EmployeeList(ListView):
+    model = Employee
+    context_object_name = 'list_employees'
+
+    def get_queryset(self):
+        return Employee.objects.all() 
